67ff4994e16eb89a2a457c223cb15a8ee9ef04ba
Rodolfo Perez Gómez <pipo6280@gmail.com>
2016-08-02 20:18:10 -0500

    se agregan las opciones al menu, iconos y frames

--- src/com/gimnasio/views/frmPaquetes.java
+++ src/com/gimnasio/views/frmPaquetes.java
@@ -126,13 +126,13 @@ public class frmPaquetes extends javax.swing.JInternalFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Title 1", "Title 2", "Title 3"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
