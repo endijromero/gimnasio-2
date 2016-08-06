9b22a146ada06c4c5fd7c185ccbdc88a290c891e
emimaster16 <emimaster16@gmail.com>
2016-08-05 22:01:22 -0500

    se modifcan los archivos de formularios

--- src/com/gimnasio/model/Model.java
+++ src/com/gimnasio/model/Model.java
@@ -1,7 +1,6 @@
 package com.gimnasio.model;
 
 import com.gimnasio.util.Util;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
