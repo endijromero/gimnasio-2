8f2df23598c7cc6761bced5db591c03ba49077ce
Rodolfo Perez Gómez <pipo6280@gmail.com>
2016-08-28 08:57:28 -0500

    ajuste par al foto en registrar pagos

--- src/com/gimnasio/util/Util.java
+++ src/com/gimnasio/util/Util.java
@@ -230,7 +230,7 @@ public class Util {
             // Util.class.getResource(ruta)
             //JasperReport load = (JasperReport) JRLoader.loadObject(fileReport.getAbsolutePath());
             JasperPrint print = JasperFillManager.fillReport(load, parametros, conexion.getConexion());
-            JasperViewer view = new JasperViewer(print);
+            JasperViewer view = new JasperViewer(print,false);
             view.setVisible(true);
             // ya cierto lo quse no se es porque cierra tod la pp igual no se por que hace eso jajja  vbouys a mirar
             conexion.close();
